2cd26d7a6a02d1469c81ea4de5ee5364e1aeed22
Júlio Soares <julio.soares@lais.huol.ufrn.br>
2024-09-05 00:37:25 -0300

    Feat: Endpoit para criar novas tasks + schema de Task

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Task(BaseModel):
+    title: str
+    description: str
+    completed: bool = False
+    
+    
+tasks = [
+        {"id": 1, "title": "task 01", "description": "descricao 01"}, 
+        {"id": 2, "title": "task 02", "description": "descricao 02"}
+    ]
 
-@app.get("/")
-def get_user():
-    return {"message": "Hello World"}
+
+@app.post("/tasks", status_code=status.HTTP_201_CREATED)
+def create_task(new_task: Task):
+    print(new_task)
+    return new_task.dict()
